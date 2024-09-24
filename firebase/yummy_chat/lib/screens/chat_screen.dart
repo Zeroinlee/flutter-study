bf455421a0b0b2a77825c457a9a1620fd5904e0f
Home_zeroinlee <nyile4423@gmail.com>
2024-09-25 03:40:03 +0900

    추가: Firebase 통합을 위한 플러그인 및 설정 추가, 채팅 화면 구현

---
+++ firebase/yummy_chat/lib/screens/chat_screen.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _authentication = FirebaseAuth.instance;
+  User? loggedUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _authentication.currentUser;
+      if (user != null) {
+        loggedUser = user;
+        print(loggedUser!.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat screen'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _authentication.signOut();
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.exit_to_app_sharp),
+            color: Colors.black,
+          )
+        ],
+      ),
+      body: Center(
+        child: Text('Welecome! ${loggedUser!.email}'),
+      ),
+    );
+  }
+}
