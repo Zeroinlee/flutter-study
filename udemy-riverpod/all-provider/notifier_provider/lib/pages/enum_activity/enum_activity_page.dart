61f7975f6048883c63a2902c253bdf49089767d1
Home_zeroinlee <nyile4423@gmail.com>
2024-10-10 12:08:03 +0900

    Add bulleted_list dependency for displaying bullet points in the app

---
+++ udemy-riverpod/all-provider/notifier_provider/lib/pages/enum_activity/enum_activity_page.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+
+import 'package:bulleted_list/bulleted_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notifier_provider/pages/enum_activity/enum_activity_provider.dart';
+import 'package:notifier_provider/pages/enum_activity/enum_activity_state.dart';
+
+import '../../models/activity.dart';
+
+class EnumActivityPage extends ConsumerStatefulWidget {
+  const EnumActivityPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _EnumActivityPageState();
+}
+
+class _EnumActivityPageState extends ConsumerState<EnumActivityPage> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(enumActivityProvider.notifier).fetchActivity(activityTypes[0]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final activityState = ref.watch(enumActivityProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Enum Activity Page'),
+      ),
+      body: switch (activityState.status) {
+        ActivityStatus.initial => const Text('Get some activity'),
+        ActivityStatus.loading => const CircularProgressIndicator(),
+        ActivityStatus.failure =>
+          activityState.activities.first == Activity.empty()
+              ? Text(activityState.error)
+              : ActivityWidget(activity: activityState.activities.first),
+        ActivityStatus.success =>
+          ActivityWidget(activity: activityState.activities.first),
+      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final randomNumber = Random().nextInt(activityTypes.length);
+          ref.read(enumActivityProvider.notifier).fetchActivity(
+                activityTypes[randomNumber],
+              );
+        },
+        child: const Text('New Activity'),
+      ),
+    );
+  }
+}
+
+class ActivityWidget extends StatelessWidget {
+  final Activity activity;
+  const ActivityWidget({
+    super.key,
+    required this.activity,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(activity.type),
+        BulletedList(listItems: [
+          'activity: ${activity.activity}',
+          'accessibility: ${activity.accessibility}',
+          'participants: ${activity.participants}',
+          'price: ${activity.price}',
+          'key: ${activity.key}',
+        ])
+      ],
+    );
+  }
+}
