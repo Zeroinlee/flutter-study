3f62687b5433127ab749f9fd66e5bae53748b477
Zeroinlee <56835122+Zeroinlee@users.noreply.github.com>
2024-10-11 15:50:07 +0900

    Delete udemy_riverpod/fb_auth_riverpod/lib/models directory

--- udemy_riverpod/fb_auth_riverpod/lib/models/app_user.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-part 'app_user.freezed.dart';
