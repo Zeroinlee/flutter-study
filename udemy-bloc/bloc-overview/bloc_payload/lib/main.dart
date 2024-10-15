147d900471b5529a6dcd34667523c1b08b78d5e2
Home_zeroinlee <nyile4423@gmail.com>
2024-10-15 11:10:05 +0900

    추가: 여러 플랫폼에 대한 .gitignore 및 xcconfig 파일 추가, README 문서 작성

---
+++ udemy-bloc/bloc-overview/bloc_payload/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'theme/theme_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            title: 'Event Payload',
+            debugShowCheckedModeBanner: false,
+            theme: state.appTheme == AppTheme.light
+                ? ThemeData.light()
+                : ThemeData.dark(),
+            home: const MyHomePage(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Theme'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            final int randInt = Random().nextInt(10);
+            print('randInt: $randInt');
+
+            context.read<ThemeBloc>().add(ChangeThemeEvent(randInt: randInt));
+          },
+          child: const Text(
+            'Change Theme',
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
