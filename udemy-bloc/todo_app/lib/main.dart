f3ffb140dc65b81764316bbe04f91f110d2beb9b
zeroinlee <zeroinlee@gmail.com>
2024-10-30 03:10:57 +0900

    추가: FilteredTodosCubit 및 관련 상태 관리 구현, 메인 앱에서 BlocProvider 설정

--- udemy-bloc/todo_app/lib/main.dart
+++ udemy-bloc/todo_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_cubit/cubits/cubits.dart';
 
 import 'pages/todos_page.dart';
 
@@ -11,14 +13,38 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODO',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodoFilterCubit>(
+          create: (context) => TodoFilterCubit(),
+        ),
+        BlocProvider<TodoSearchCubit>(
+          create: (context) => TodoSearchCubit(),
+        ),
+        BlocProvider<TodoListCubit>(
+          create: (context) => TodoListCubit(),
+        ),
+        BlocProvider<ActiveTodoCountCubit>(
+          create: (context) => ActiveTodoCountCubit(
+              todoListCubit: context.read<TodoListCubit>()),
+        ),
+        BlocProvider<FilteredTodosCubit>(
+          create: (context) => FilteredTodosCubit(
+            todoFilterCubit: context.read<TodoFilterCubit>(),
+            todoListCubit: context.read<TodoListCubit>(),
+            todoSearchCubit: context.read<TodoSearchCubit>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TODO',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const TodosPage(),
       ),
-      home: const TodosPage(),
     );
   }
 }
